7487a32aa51b1f7f08e7fa0dc87ad3467cd922e9
Ryan Jordan <ryjordan@gmail.com>
2016-05-29 20:53:51 -0500

    Add Book preview component styles

--- src/components/book-preview.ts
+++ src/components/book-preview.ts
@@ -1,38 +1,53 @@
 import { Component, Input } from '@angular/core';
 
 import { Book } from '../models';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 
 
 export type BookInput = Book;
 
 @Component({
   selector: 'book-preview',
+  directives: [
+    ...MD_CARD_DIRECTIVES,
+    ...MD_LIST_DIRECTIVES
+  ],
   template: `
-    <a [linkTo]=" '/book/' + id ">
-      <h3>{{ title }}</h3>
+    <a [linkTo]=" '/book/' + id">
+      <md-card>
+        <md-card-title-group>
+          <md-card-title>{{ title }}</md-card-title>
+          <md-card-subtitle *ngIf="subtitle">{{ subtitle }}</md-card-subtitle>
+          <img md-card-sm-image *ngIf="thumbnail" [src]="thumbnail"/>
+        </md-card-title-group>
+        <md-card-content>
+          <p *ngIf="description">{{ description | slice: 0:250 }}{{ description.length > 20 ? '...' : '' }}</p>
+        </md-card-content>
+        <md-list dense>
+          <h3 md-subheader>Written By:</h3>
+          <md-list-item *ngFor="let author of authors">
+            <h4 md-line>{{ author }}</h4>
+          </md-list-item>
+        </md-list>
+      </md-card>
     </a>
-
-    <h4 *ngIf="subtitle">{{ subtitle }}</h4>
-
-    <img *ngIf="thumbnail" [src]="thumbnail">
-
-    <p>{{ description }}</p>
-
-    <div>
-      Written By:
-      <ul>
-        <li *ngFor="let author of authors">{{ author }}</li>
-      </ul>
-    </div>
   `,
   styles: [`
-    :host {
-      display: block;
-      width: 300px;
-      padding: 20px;
-      border: 1px solid black;
-      margin: 10px;
-      cursor: pointer;
+    md-card {
+      width: 400px;
+      box-sizing: border-box;
+      margin: 16px;
+    }
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+    img {
+      width: 60px;
+    }
+    md-card-content {
+      margin-top: 10px;
     }
   `]
 })
