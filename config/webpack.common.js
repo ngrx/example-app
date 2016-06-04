4659d692380eeb97492cba7fd3979cd85863adfb
Kapil Sachdeva <ksachdeva17@gmail.com>
2016-06-04 18:12:29 -0500

    chore - correct the name of the author and various urls related to this repository

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -17,7 +17,7 @@ const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin
  * Webpack Constants
  */
 const METADATA = {
-  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  title: 'An example application showing usage of varios @ngrx modules',
   baseUrl: '/'
 };
 
