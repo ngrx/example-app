482841b550f243edffe5dd98a0b567c0bb9f6558
Ilya Hrytsuk <ilya.hrytsuk@rubyroidlabs.com>
2016-11-14 11:05:11 +0300

    fix(GoogleBooksService): If api responds with zero books found.

--- src/app/services/google-books.ts
+++ src/app/services/google-books.ts
@@ -13,7 +13,7 @@ export class GoogleBooksService {
 
   searchBooks(queryTitle: string): Observable<Book[]> {
     return this.http.get(`${this.API_PATH}?q=${queryTitle}`)
-      .map(res => res.json().items);
+      .map(res => res.json().items || []);
   }
 
   retrieveBook(volumeId: string): Observable<Book> {
